a680763d3509f81452e777fb379fc9ae8e04aee6
Alexander Goussas <goussasalexander@gmail.com>
2025-04-19 21:31:59 -0500

    cdc-consumer: add websocket server

--- cdc-consumer/index.js
+++ cdc-consumer/index.js
@@ -1,12 +1,31 @@
 const { Kafka } = require('kafkajs');
+const { WebSocketServer } = require('ws');
 
 const topic = process.env.KAFKA_TOPIC || 'cdc-changes';
-
 const brokerAddr = process.env.KAFKA_BROKER_ADDR || 'localhost:9092';
+const serverPort = process.env.WS_SERVER_PORT || 8080;
+
+const wss = new WebSocketServer({ port: serverPort });
 
 const kafka = new Kafka({
   clientId: 'cdc-consumer',
   brokers: [brokerAddr]
+})
+
+const connections = new Set();
+
+wss.on('connection', (ws) => {
+  connections.add(ws);
+
+  ws.on('close', () => {
+    console.log('Client disconnected');
+    connections.delete(ws);
+  })
+
+  ws.on('error', (err) => {
+    console.log('WS error', err);
+    connections.delete(ws);
+  })
 })
 
 
@@ -24,6 +43,11 @@ const consumer = kafka.consumer({
 
   await consumer.run({
     eachMessage: async ({ message }) => {
+      if (connections.size === 0) {
+        console.log('No clients connected');
+        return;
+      }
+
       const msg = message.value.toString()
       const json = JSON.parse(msg).change[0]
       const payload = {
@@ -33,7 +57,9 @@ const consumer = kafka.consumer({
         values: json.columnvalues,
       }
 
-      // TODO: Send over WebSocket
+      for (const ws of connections) {
+        ws.send(JSON.stringify(payload));
+      }
     },
   })
 })()
