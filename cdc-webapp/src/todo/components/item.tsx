955cc7786f5ab0be7874efca66013480c6ba0fb8
Alexander Goussas <goussasalexander@gmail.com>
2025-05-01 15:53:41 -0500

    feat: use API endpoints in item component

--- cdc-webapp/src/todo/components/item.tsx
+++ cdc-webapp/src/todo/components/item.tsx
@@ -4,50 +4,100 @@ import classnames from "classnames";
 import { Input } from "./input";
 
 import { TOGGLE_ITEM, REMOVE_ITEM, UPDATE_ITEM } from "../constants";
+import { useServerUrl } from "../../hooks/useServerUrl";
 
 export const Item = memo(function Item({ todo, dispatch, index }) {
-    const [isWritable, setIsWritable] = useState(false);
-    const { title, completed, id } = todo;
+  const [isWritable, setIsWritable] = useState(false);
+  const { title, completed, id } = todo;
+  const { serverUrl } = useServerUrl();
 
-    const toggleItem = useCallback(() => dispatch({ type: TOGGLE_ITEM, payload: { id } }), [dispatch]);
-    const removeItem = useCallback(() => dispatch({ type: REMOVE_ITEM, payload: { id } }), [dispatch]);
-    const updateItem = useCallback((id, title) => dispatch({ type: UPDATE_ITEM, payload: { id, title } }), [dispatch]);
+  const toggleItem = useCallback(async () => {
+    await fetch(`${serverUrl}/todos/${id}/toggle`, {
+      method: "POST",
+    });
 
-    const handleDoubleClick = useCallback(() => {
-        setIsWritable(true);
-    }, []);
+    dispatch({ type: TOGGLE_ITEM, payload: { id } });
+  }, [dispatch]);
 
-    const handleBlur = useCallback(() => {
-        setIsWritable(false);
-    }, []);
+  const removeItem = useCallback(async () => {
+    await fetch(`${serverUrl}/todos/${id}`, {
+      method: "DELETE",
+    });
 
-    const handleUpdate = useCallback(
-        (title) => {
-            if (title.length === 0)
-                removeItem(id);
-            else
-                updateItem(id, title);
+    dispatch({ type: REMOVE_ITEM, payload: { id } });
+  }, [dispatch]);
 
-            setIsWritable(false);
+  const updateItem = useCallback(
+    async (id: number, title: string) => {
+      await fetch(`${serverUrl}/todos/${id}`, {
+        method: "PATCH",
+        body: JSON.stringify({
+          title,
+        }),
+        headers: {
+          "Content-Type": "application/json",
         },
-        [id, removeItem, updateItem]
-    );
+      });
 
-    return (
-        <li className={classnames({ completed: todo.completed })} data-testid="todo-item">
-            <div className="view">
-                {isWritable ? (
-                    <Input onSubmit={handleUpdate} label="Edit Todo Input" defaultValue={title} onBlur={handleBlur} />
-                ) : (
-                    <>
-                        <input className="toggle" type="checkbox" data-testid="todo-item-toggle" checked={completed} onChange={toggleItem} />
-                        <label data-testid="todo-item-label" onDoubleClick={handleDoubleClick}>
-                            {title}
-                        </label>
-                        <button className="destroy" data-testid="todo-item-button" onClick={removeItem} />
-                    </>
-                )}
-            </div>
-        </li>
-    );
+      dispatch({ type: UPDATE_ITEM, payload: { id, title } });
+    },
+    [dispatch],
+  );
+
+  const handleDoubleClick = useCallback(() => {
+    setIsWritable(true);
+  }, []);
+
+  const handleBlur = useCallback(() => {
+    setIsWritable(false);
+  }, []);
+
+  const handleUpdate = useCallback(
+    (title) => {
+      if (title.length === 0) removeItem(id);
+      else updateItem(id, title);
+
+      setIsWritable(false);
+    },
+    [id, removeItem, updateItem],
+  );
+
+  return (
+    <li
+      className={classnames({ completed: todo.completed })}
+      data-testid="todo-item"
+    >
+      <div className="view">
+        {isWritable ? (
+          <Input
+            onSubmit={handleUpdate}
+            label="Edit Todo Input"
+            defaultValue={title}
+            onBlur={handleBlur}
+          />
+        ) : (
+          <>
+            <input
+              className="toggle"
+              type="checkbox"
+              data-testid="todo-item-toggle"
+              checked={completed}
+              onChange={toggleItem}
+            />
+            <label
+              data-testid="todo-item-label"
+              onDoubleClick={handleDoubleClick}
+            >
+              {title}
+            </label>
+            <button
+              className="destroy"
+              data-testid="todo-item-button"
+              onClick={removeItem}
+            />
+          </>
+        )}
+      </div>
+    </li>
+  );
 });
